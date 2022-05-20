3cbaef6b6640dd30908f9037008e5c2e4601cd0b
laskor <al.moksud@gmail.com>
2022-05-20 02:02:36 -0400

    posting new user

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
-import { useEffect, useState, useSyncExternalStore } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [users, setUsers] = useState([]);
+
   useEffect(() => {
-    fetch('http://localhost:5000/users')
-      .then(res => res.json())
-      .then(data => setUsers(data));
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
   }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = { name, email };
+
+    // post data to server
+    fetch("http://localhost:5000/user", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const newUser = [...users, data];
+        setUsers(newUser);
+        //console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
   return (
     <div className="App">
       <h1>My own data : {users.length}</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Name" />
+        <input type="email" name="email" placeholder="Email" />
+        <button type="submit">Add user</button>
+      </form>
       <ul>
-      {
-        users.map(user => <li key={user.id}>id: {user.id} name : {user.name} email: {user.email}</li>)
-      }
+        {users.map((user) => (
+          <li key={user.id}>
+            id: {user.id} name : {user.name} email: {user.email}
+          </li>
+        ))}
       </ul>
     </div>
   );
