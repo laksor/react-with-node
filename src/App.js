523a6603929e503aef6ad4de33128a9ce0878d19
laskor <al.moksud@gmail.com>
2022-05-20 00:10:36 -0400

    users loaded from own server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import { useEffect, useState, useSyncExternalStore } from "react";
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []);
   return (
     <div className="App">
-      
+      <h1>My own data : {users.length}</h1>
+      <ul>
+      {
+        users.map(user => <li key={user.id}>id: {user.id} name : {user.name} email: {user.email}</li>)
+      }
+      </ul>
     </div>
   );
 }
